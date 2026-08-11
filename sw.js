d881f885657cb214ab9ccb4b3ba69c4e9b4919b7
Taha Oruç <taha.oruc@outlook.de>
2026-08-12 00:11:57 +0200

    Arabische Morphologie: Konjugationstrainer als PWA
    Interaktives Lerntool fuer arabische Verbkonjugation (Kitab Al-Binaa,
    Bab 1-6) mit Tabellen-Layout, Vergangenheit/Gegenwart, Aktiv/Passiv,
    Vokabelmodus und Fortschrittsanzeige. Installierbar als App via
    "Zum Home-Bildschirm hinzufuegen".

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'arabisch-morphologie-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
